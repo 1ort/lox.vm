f7776c83399bd366f747d3eb3d00643f000a9807
1ort <83316072+1ort@users.noreply.github.com>
2026-06-22 02:34:03 +0500

    ch26: garbage collector api

---
+++ gc/src/lib.rs
@@ -0,0 +1,69 @@
+use std::{cell::Cell, marker::PhantomData};
+
+/// Traceble type for mark&sweep GC
+pub trait Trace {
+    /// Implementation must recursively call Trace::trace() on it's every field
+    fn trace(&self);
+}
+
+/// Shared ownership of a value on gc-heap
+pub struct Gc<T: Trace> {
+    index: usize,
+    marker_: PhantomData<T>,
+}
+
+/// Value on gc-heap
+struct GcBox<T: Trace> {
+    value: T,
+    /// this field is used to mark object as accessed from root.
+    accessed: Cell<bool>,
+}
+
+/// Virtual heap with garbage-collection
+pub struct GcHeap<T: Trace> {
+    /// Every Value allocated on this heap
+    values: Vec<Option<GcBox<T>>>,
+    /// stack of weakref indexes which values were dropped
+    free_indexes: Vec<usize>,
+}
+
+impl<T: Trace> GcHeap<T> {
+    fn add(&mut self, value: T) -> Gc<T> {
+        let gcbox = GcBox {
+            value,
+            accessed: Cell::new(false),
+        };
+
+        let index = {
+            match self.free_indexes.pop() {
+                Some(index) => {
+                    self.values[index].replace(gcbox);
+                    index
+                }
+                None => {
+                    self.values.push(Some(gcbox));
+                    self.values.len() - 1
+                }
+            }
+        };
+
+        Gc {
+            index,
+            marker_: PhantomData,
+        }
+    }
+
+    fn get(&self, gc: Gc<T>) -> &T {
+        &self.values[gc.index]
+            .as_ref()
+            .expect("Dangling GCHeap pointer")
+            .value
+    }
+
+    fn get_mut(&mut self, gc: Gc<T>) -> &mut T {
+        &mut self.values[gc.index]
+            .as_mut()
+            .expect("Dangling GCHEap pointer")
+            .value
+    }
+}
